92bd09441e494f4b073e2bc4036913dcde867e92
Paul-Messina <Paul.messina16@gmail.com>
2019-03-26 14:50:51 -0400

    Creation of User.java

---
+++ User.java
@@ -0,0 +1,19 @@
+import java.util.ArrayList;
+
+public class User {
+    
+    private String userName;
+    private ArrayList<Playlist> playlists;
+
+
+    public User(){
+
+        userName = "John Doe";
+        playlists = new ArrayList<Playlist>();
+
+    }
+
+
+
+
+}
