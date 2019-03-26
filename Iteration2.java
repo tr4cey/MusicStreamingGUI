ebceca65f86717f93f531168086aa02c0ac54e61
Ryan <ryan.tracey@ryerson.ca>
2019-03-26 13:20:04 -0400

    First file to start the assignment doesnt work tho

---
+++ Iteration2.java
@@ -0,0 +1,12 @@
+import javax.swing.JFrame;
+
+public class Iteration2
+{  
+   public static void main(String[] args)
+   {  
+      JFrame frame = new //Name of class();
+      frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+      frame.setTitle("name of program");
+      frame.setVisible(true);      
+   }
+}
