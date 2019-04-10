0813b84fbb22263997f1c2cc73865ce1733cd0ad
Maxmets <maxmets27@gmail.com>
2019-04-10 11:57:45 -0400

    Added comments to classes User and Song

--- Song.java
+++ Song.java
@@ -6,6 +6,14 @@ public class Song
     int year, secLength = 0;
     
 
+    /**
+     * Creates a song object that is to be added into a playlist
+     * @param name the name of the song 
+     * @param artist the artist who created the song 
+     * @param year the year the song was released 
+     * @param secLength the song length in seconds 
+     * @param genre the genre of the song 
+     */
     Song(String name, String artist, int year, int secLength, String genre)
     {
         this.name = name;
@@ -15,27 +23,53 @@ public class Song
         this.genre = genre;
     }
 
+    /**
+     * Returns the name of the artist 
+     * @return the name of the artist 
+     */
     public String getArtist()
     {
         return artist;
     }
+
+    /**
+     * Returns the name of the song 
+     * @return the name of the song 
+     */
     public String getName()
     {
         return name;
     }
+
+    /**
+     * Gets the genre of the song 
+     * @return the genre of the song 
+     */
     public String getGenre()
     {
         return genre;
     }
+    /**
+     * Returns the year the song was released 
+     * @return the year the song was released
+     */
     public int getYear()
     {
         return year;
     }
+    /**
+     * Gets the length of the song in seconds 
+     * @return the length of the song in seconds 
+     */
     public int getLengthSec()
     {
         return secLength;
     }
 
+    /**
+     * Returns the length of the song in minutes 
+     * @return the length of the song
+     */
     public String getLengthMin()
     {
         String minSec = (secLength/60) + "";
--- User.java
+++ User.java
@@ -5,34 +5,63 @@ public class User {
     private String userName;
     private ArrayList<Playlist> playlists;
 
-
+    /**
+     * The user that will be making the 
+     * playlists 
+     */
     public User(){
 
         userName = "John Doe";
         playlists = new ArrayList<Playlist>();
     }
 
+    /**
+     * Creates a user with a username already defined 
+     * @param userName the name of the user
+     */
     public User(String userName){
         this.userName = userName;
         playlists = new ArrayList<Playlist>();
     }
 
+    /**
+     * Gets the username of the user 
+     * @return the username 
+     */
     public String getUserName(){
         return this.userName;
     }
 
+    /**
+     * Returns all the playlists of the user 
+     */
     public ArrayList<Playlist> getAllPlaylists(){
         return this.playlists;
     }
 
+    /**
+     * Gets a particular playlist from all 
+     * the playlists that the user has 
+     * @param index the particular playlist to be selected 
+     * @return the playlist that the user wants
+     */
     public Playlist getPlaylist(int index){
         return this.playlists.get(index);
     }
 
+    /**
+     * Creates a playlist that is then 
+     * added to the list of user's other playlists
+     */
     public void makePlaylist(){
         this.playlists.add(new Playlist());
     }
 
+    /**
+     * Removes a particular playlist from the user's 
+     * plaulist list. 
+     * @param playlist the particular playlist to be removed
+     */
     public void removePlaylist(Playlist playlist){
         this.playlists.remove(playlist);
     }
